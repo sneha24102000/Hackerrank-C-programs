1ff42d3f441796f44cd64c243924c787b445c175
Sneha M <67436805+sneha24102000@users.noreply.github.com>
2021-02-27 18:28:54 +0530

    Add files via upload

---
+++ StudentsMarksSumProblem.c
@@ -0,0 +1,20 @@
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int sum=0;
+  
+  if(gender=='b')
+  {
+      for(int i=0;i<number_of_students; i++,++i)
+      {
+          sum=sum+marks[i];
+      }
+  }
+  else {
+      for(int i=1;i<number_of_students; i++,++i)
+      {
+          sum=sum+marks[i];
+      }
+    
+  }
+  return sum;
+}
